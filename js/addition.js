e1f9fe2fadf12b0bc56708780da25e0904f36226
sheenashahangian <sheenas@bu.edu>
2021-11-19 08:32:25 -0700

    begin working on js

---
+++ js/addition.js
@@ -0,0 +1,17 @@
+
+function addNumbers(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+
+function isNumberBig(number) {
+
+}
+
+function calculatorRunner() {
+    // welcome visitor with an alert
+    alert("Welcome to the Simple Calculator by Sheena!");
+
+    // prompt the user for their name
+    prompt("What is your name?");
+
+}
